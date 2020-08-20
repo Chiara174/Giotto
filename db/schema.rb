a4acdb5f42b19ebbf00879f1120b4c1962b98393
Chiara Gerlach <chiara.gerlach@online.de>
2020-08-20 17:32:30 +0200

    Added booking form and worked on controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_19_102146) do
+ActiveRecord::Schema.define(version: 2020_08_20_151923) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,11 +39,11 @@ ActiveRecord::Schema.define(version: 2020_08_19_102146) do
   create_table "bookings", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "furniture_id", null: false
-    t.integer "start_date"
-    t.integer "end_date"
     t.string "status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.date "start_date"
+    t.date "end_date"
     t.index ["furniture_id"], name: "index_bookings_on_furniture_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
