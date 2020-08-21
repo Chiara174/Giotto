03a90030c8f0926be9437a03c78efd3e38083096
Chiara Gerlach <chiara.gerlach@online.de>
2020-08-21 11:31:01 +0200

    Ran migrations and deleted duplicated files

--- db/migrate/20200820135648_add_coordinates_to_furnitures.rb
+++
@@ -1,6 +0,0 @@
-class AddCoordinatesToFurnitures < ActiveRecord::Migration[6.0]
-  def change
-    add_column :furnitures, :latitude, :float
-    add_column :furnitures, :longitude, :float
-  end
-end
